7e486af0606b2d930cb99adbac5a4cd6efdc0b86
Varsha Reddy <ushasri.dasari92@gmail.com>
2025-01-12 19:31:08 +0530

    Valid Palindrome

---
+++ Two_Pointers/125_Valid_Palindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+    #returning alphanumeric value
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
+        #solution2
+        Alphanum = ""
+        if s == " ":
+            return True
+        for c in s:
+            if c.isalnum():
+                Alphanum += c.lower()
+        #print(Alphanum)
+        #print(Alphanum[::-1])
+        if Alphanum == Alphanum[::-1]:
+            #print(Alphanum)
+            return True
+        return False
+
