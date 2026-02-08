add6936352d1f9556a45acdffccdb8c4edeb1643
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-02-08 11:11:42 +0530

    Time: 1 ms (88.74%), Space: 21.2 MB (51.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        #sorting all the numbers in an array
         nums.sort()
+        #returning the middle element as it appears more that n/2 times
         return nums[len(nums) // 2]
                     
             
