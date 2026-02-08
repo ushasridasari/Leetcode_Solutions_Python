eb32836102eeab2700221c4f0638a8a8b39bc486
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-02-08 10:39:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (34.62%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -8,8 +8,11 @@ class Solution:
         for i in range(n):
             # value at index is not equals to 3
             if nums[i] != val:
+                #adding the value to array
                 arr.append(nums[i])
+                #increasing the len of the arr
                 k += 1
+        #copying all the elements to nums as we need to return nums len
         for i in range(len(arr)):
             nums[i] = arr[i]
         return k
