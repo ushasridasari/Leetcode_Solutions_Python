ba1dbe82aeb409d71be1708f956a5a5cbdc5f009
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-04-19 11:16:19 -0700

    Time: 43 ms (62.61%), Space: 34.1 MB (60.7%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,38 +1,42 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        #Stores final answers
-        res = []
-        #Stores current values of the string
-        path = []
-        #Starts checking from index i
-        def dfs(i):
-            #If we reach end, Save current path and Stop
-            if i >= len(s):
-                res.append(path.copy())
-                return
-            #loop through each indeax of str
-            for j in range(i, len(s)):
-                #Only continue if substring is palindrome
-                if self.isPalin(s, i, j):
-                    #Add current part
-                    path.append(s[i:j+1])
-                    #Move to next path
-                    dfs(j+1)
-                    #Remove last part
-                    path.pop()
+        #res to store the result and part to store the curr path
+        res, part = [], []
 
+        #
+        def dfs(i):
+            #If we reached end of string
+            if i >= len(s):
+                #Save current partition into result
+                res.append(part.copy())
+                #Stops this path
+                return
+            #Trying all substrings starting from index i
+            for j in range(i, len(s)):
+                #Check if substring s[i:j+1] is palindrome
+                if self.isPalin(s, i, j):
+                    #Add this piece to current path
+                    part.append(s[i : j+1])
+                    #Move to next part of string
+                    dfs(j+1)
+                    #Remove last added piece to try another option
+                    part.pop()
+
+        #Start from index 0
         dfs(0)
+        #Returning all the partitions
         return res
-    #Checks if substring is palindrome
+
     def isPalin(self, s, l, r):
+        #Compare from both ends
         while l < r:
-            #If letters don’t match then not palindrome
+            #If the left and right letters don't match return false
             if s[l] != s[r]:
                 return False
             #Move inward
-            l, r = l +1, r -1
+            l, r = l+1, r-1
+        #If all matched then it is a palindrome
         return True
-    #TC: O(n*2^n)
-    #SC: O(n*2^n)
 
-        
+#TC: O(n*2^n)
+#SC: O(n) and O(n*2^n)
