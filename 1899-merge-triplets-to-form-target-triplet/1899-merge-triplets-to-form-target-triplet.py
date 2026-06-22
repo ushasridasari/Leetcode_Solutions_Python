d729f9816a68ff7407afcbbc5efea8e7237a1cda
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-06-22 07:44:57 +0530

    Time: 0 ms (100%), Space: 54 MB (21.47%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -6,7 +6,9 @@ class Solution:
         for t in triplets:
             #Verify:t[0] == target[0] whether the first number matches the target's first number;      t[1], t[2] is less than their respective targets. if Ture -> x = X or True 
             x |= (t[0] == target[0] and t[1] <= target[1] and t[2] <= target[2])
+            #y becomes True if a triplet has the second target value exactly, while the other two values are not greater than the target.
             y |= (t[0] <= target[0] and t[1] == target[1] and t[2] <= target[2])
+            #z becomes True if a triplet has the third target value exactly, while the other two values are not greater than the target.
             z |= (t[0] <= target[0] and t[1] <= target[1] and t[2] == target[2])
             #if all are true, then we can build a triplet
             if x and y and z:
