796f335efee9d27e257a5e69a5a661d0db8441d2
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-06-28 19:40:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (29.96%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+        #Keep rotating until all layers are processed.
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                # save the topleft
+                topLeft = matrix[top][l + i]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+            r -= 1
+            l += 1
+
+    #TC: O(n^2)
+    #SC: O(1)
