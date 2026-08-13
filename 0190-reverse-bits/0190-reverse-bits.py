98fc2d1ce766c78356a682373c4acf646cf21f52
Ushasri Dasari <ushasri.dasari92@gmail.com>
2026-08-12 17:41:23 -0700

    Time: 50 ms (31.54%), Space: 19.2 MB (64.13%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            #Gets the i-th bit from n.
+            bit = (n >> i) & 1
+            #Moving that bit to the opposite side i.e, Moves that bit to its reversed position in res.
+            res |= (bit << (31 - i))
+        return res
+        
+        
